f404c8dd22562dcd9d41950c856f590094bb32f1
MizuchiKun <til@jungbluths.de>
2025-04-24 17:51:50 +0200

    Created test scene and added YT player controller.
    - Added grass rule tile and made test 'level'.
    - Added Ile's GameObject with Rb and collider.
    - Added and (somewhat) configured player movement script from YT
      (which I'll likely replace/refactor).

---
+++ Assets/Scripts/PlayerMovementYT.cs
@@ -0,0 +1,361 @@
+using System.Collections;
+using UnityEditor.TextCore.Text;
+using UnityEngine;
+
+// Source: https://www.youtube.com/watch?v=KbtcEVCM7bw.
+public class PlayerMovementYT : MonoBehaviour
+{
+    #region Inspector parameters.
+    [Header("Gravity")]
+    [HideInInspector] public float gravityStrength; //Downwards force (gravity) needed for the desired jumpHeight and jumpTimeToApex.
+    [HideInInspector] public float gravityScale; //Strength of the player's gravity as a multiplier of gravity (set in ProjectSettings/Physics2D).
+                                                 //Also the value the player's rigidbody2D.gravityScale is set to.
+    [Space(5)]
+    public float fallGravityMult; //Multiplier to the player's gravityScale when falling.
+    public float maxFallSpeed; //Maximum fall speed (terminal velocity) of the player when falling.
+    [Space(5)]
+    public float fastFallGravityMult; //Larger multiplier to the player's gravityScale when they are falling and a downwards input is pressed.
+                                      //Seen in games such as Celeste, lets the player fall extra fast if they wish.
+    public float maxFastFallSpeed; //Maximum fall speed(terminal velocity) of the player when performing a faster fall.
+
+    [Space(20)]
+
+    [Header("Run")]
+    public float runMaxSpeed; //Target speed we want the player to reach.
+    public float runAcceleration; //The speed at which our player accelerates to max speed, can be set to runMaxSpeed for instant acceleration down to 0 for none at all
+    [HideInInspector] public float runAccelAmount; //The actual force (multiplied with speedDiff) applied to the player.
+    public float runDecceleration; //The speed at which our player decelerates from their current speed, can be set to runMaxSpeed for instant deceleration down to 0 for none at all
+    [HideInInspector] public float runDeccelAmount; //Actual force (multiplied with speedDiff) applied to the player .
+    [Space(5)]
+    [Range(0f, 1)] public float accelInAir; //Multipliers applied to acceleration rate when airborne.
+    [Range(0f, 1)] public float deccelInAir;
+    [Space(5)]
+    public bool doConserveMomentum = true;
+
+    [Space(20)]
+
+    [Header("Jump")]
+    public float jumpHeight; //Height of the player's jump
+    public float jumpTimeToApex; //Time between applying the jump force and reaching the desired jump height. These values also control the player's gravity and jump force.
+    [HideInInspector] public float jumpForce; //The actual force applied (upwards) to the player when they jump.
+    public float jumpCutGravityMult; //Multiplier to increase gravity if the player releases thje jump button while still jumping
+    [Range(0f, 1)] public float jumpHangGravityMult; //Reduces gravity while close to the apex (desired max height) of the jump
+    public float jumpHangTimeThreshold; //Speeds (close to 0) where the player will experience extra "jump hang". The player's velocity.y is closest to 0 at the jump's apex (think of the gradient of a parabola or quadratic function)
+    [Space(0.5f)]
+    public float jumpHangAccelerationMult;
+    public float jumpHangMaxSpeedMult;
+
+    [Space(20)]
+
+    [Header("Assists")]
+    [Range(0.01f, 0.5f)] public float coyoteTime; //Grace period after falling off a platform, where you can still jump
+    [Range(0.01f, 0.5f)] public float jumpInputBufferTime; //Grace period after pressing jump where a jump will be automatically performed once the requirements (eg. being grounded) are met.
+    #endregion.
+
+    #region COMPONENTS
+    public Rigidbody2D RB { get; private set; }
+    #endregion
+
+    #region STATE PARAMETERS
+    //Variables control the various actions the player can perform at any time.
+    //These are fields which can are public allowing for other sctipts to read them
+    //but can only be privately written to.
+    public bool IsFacingRight { get; private set; }
+    public bool IsJumping { get; private set; }
+
+    //Timers (also all fields, could be private and a method returning a bool could be used)
+    public float LastOnGroundTime { get; private set; }
+
+    //Jump
+    private bool _isJumpCut;
+    private bool _isJumpFalling;
+    #endregion
+
+    #region INPUT PARAMETERS
+    private Vector2 _moveInput;
+
+    public float LastPressedJumpTime { get; private set; }
+    #endregion
+
+    #region CHECK PARAMETERS
+    //Set all of these up in the inspector
+    [Header("Checks")]
+    [SerializeField] private Transform _groundCheckPoint;
+    //Size of groundCheck depends on the size of your character generally you want them slightly small than width (for ground) and height (for the wall check)
+    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.49f, 0.03f);
+    #endregion
+
+    #region LAYERS & TAGS
+    [Header("Layers & Tags")]
+    [SerializeField] private LayerMask _groundLayer;
+    #endregion
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        SetGravityScale(gravityScale);
+        IsFacingRight = true;
+    }
+
+    private void Update()
+    {
+        #region TIMERS
+        LastOnGroundTime -= Time.deltaTime;
+
+        LastPressedJumpTime -= Time.deltaTime;
+        #endregion
+
+        #region INPUT HANDLER
+        _moveInput.x = Input.GetAxisRaw("Horizontal");
+        _moveInput.y = Input.GetAxisRaw("Vertical");
+
+
+        if (_moveInput.x != 0)
+            CheckDirectionToFace(_moveInput.x > 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            OnJumpInput();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            OnJumpUpInput();
+        }
+        #endregion
+
+        #region COLLISION CHECKS
+        if (!IsJumping)
+        {
+            //Ground Check
+            if (Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer)) //checks if set box overlaps with ground
+            {
+                LastOnGroundTime = coyoteTime; //if so sets the lastGrounded to coyoteTime
+            }
+        }
+        #endregion
+
+        #region JUMP CHECKS
+        if (IsJumping && RB.linearVelocity.y < 0)
+        {
+            IsJumping = false;
+            _isJumpFalling = true;
+        }
+
+        if (LastOnGroundTime > 0 && !IsJumping)
+        {
+            _isJumpCut = false;
+            _isJumpFalling = false;
+        }
+
+        //Jump
+        if (CanJump() && LastPressedJumpTime > 0)
+        {
+            IsJumping = true;
+            _isJumpCut = false;
+            _isJumpFalling = false;
+            Jump();
+        }
+        #endregion
+
+        #region GRAVITY
+        //Higher gravity if we've released the jump input or are falling
+        if (RB.linearVelocity.y < 0 && _moveInput.y < 0)
+        {
+            //Much higher gravity if holding down
+            SetGravityScale(gravityScale * fastFallGravityMult);
+            //Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+            RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -maxFastFallSpeed));
+        }
+        else if (_isJumpCut)
+        {
+            //Higher gravity if jump button released
+            SetGravityScale(gravityScale * jumpCutGravityMult);
+            RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -maxFallSpeed));
+        }
+        else if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.linearVelocity.y) < jumpHangTimeThreshold)
+        {
+            SetGravityScale(gravityScale * jumpHangGravityMult);
+        }
+        else if (RB.linearVelocity.y < 0)
+        {
+            //Higher gravity if falling
+            SetGravityScale(gravityScale * fallGravityMult);
+            //Caps maximum fall speed, so when falling over large distances we don't accelerate to insanely high speeds
+            RB.linearVelocity = new Vector2(RB.linearVelocity.x, Mathf.Max(RB.linearVelocity.y, -maxFallSpeed));
+        }
+        else
+        {
+            //Default gravity if standing on a platform or moving upwards
+            SetGravityScale(gravityScale);
+        }
+        #endregion
+    }
+
+    private void FixedUpdate()
+    {
+        //Handle Run
+        Run(1);
+    }
+
+    #region INPUT CALLBACKS
+    //Methods which whandle input detected in Update()
+    public void OnJumpInput()
+    {
+        LastPressedJumpTime = jumpInputBufferTime;
+    }
+
+    public void OnJumpUpInput()
+    {
+        if (CanJumpCut())
+            _isJumpCut = true;
+    }
+    #endregion
+
+    #region GENERAL METHODS
+    public void SetGravityScale(float scale)
+    {
+        RB.gravityScale = scale;
+    }
+    #endregion
+
+    //MOVEMENT METHODS
+    #region RUN METHODS
+    private void Run(float lerpAmount)
+    {
+        //Calculate the direction we want to move in and our desired velocity
+        float targetSpeed = _moveInput.x * runMaxSpeed;
+        //We can reduce our control using Lerp() this smooths changes to our direction and speed
+        targetSpeed = Mathf.Lerp(RB.linearVelocity.x, targetSpeed, lerpAmount);
+
+        #region Calculate AccelRate
+        float accelRate;
+
+        //Gets an acceleration value based on if we are accelerating (includes turning)
+        //or trying to decelerate (stop). As well as applying a multiplier if we're air borne.
+        if (LastOnGroundTime > 0)
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount : runDeccelAmount;
+        else
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? runAccelAmount * accelInAir : runDeccelAmount * deccelInAir;
+        #endregion
+
+        #region Add Bonus Jump Apex Acceleration
+        //Increase our acceleration and maxSpeed when at the apex of the jump, makes the jump feel a bit more bouncy, responsive and natural
+        if ((IsJumping || _isJumpFalling) && Mathf.Abs(RB.linearVelocity.y) < jumpHangTimeThreshold)
+        {
+            accelRate *= jumpHangAccelerationMult;
+            targetSpeed *= jumpHangMaxSpeedMult;
+        }
+        #endregion
+
+        #region Conserve Momentum
+        //We won't slow the player down if they are moving in their desired direction but at a greater speed than their maxSpeed
+        if (doConserveMomentum && Mathf.Abs(RB.linearVelocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(RB.linearVelocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && LastOnGroundTime < 0)
+        {
+            //Prevent any deceleration from happening, or in other words conserve are current momentum
+            //You could experiment with allowing for the player to slightly increae their speed whilst in this "state"
+            accelRate = 0;
+        }
+        #endregion
+
+        //Calculate difference between current velocity and desired velocity
+        float speedDif = targetSpeed - RB.linearVelocity.x;
+
+        //Calculate force along x-axis to apply to the player
+        float movement = speedDif * accelRate;
+
+        //Convert this to a vector and apply to rigidbody
+        RB.AddForce(movement * Vector2.right, ForceMode2D.Force);
+
+        /*
+		 * For those interested here is what AddForce() will do
+		 * RB.linearVelocity = new Vector2(RB.linearVelocity.x + (Time.fixedDeltaTime  * speedDif * accelRate) / RB.mass, RB.linearVelocity.y);
+		 * Time.fixedDeltaTime is by default in Unity 0.02 seconds equal to 50 FixedUpdate() calls per second
+		*/
+    }
+
+    private void Turn()
+    {
+        //stores scale and flips the player along the x axis,
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+
+        IsFacingRight = !IsFacingRight;
+    }
+    #endregion
+
+    #region JUMP METHODS
+    private void Jump()
+    {
+        //Ensures we can't call Jump multiple times from one press
+        LastPressedJumpTime = 0;
+        LastOnGroundTime = 0;
+
+        #region Perform Jump
+        //We increase the force applied if we are falling
+        //This means we'll always feel like we jump the same amount
+        //(setting the player's Y velocity to 0 beforehand will likely work the same, but I find this more elegant :D)
+        float force = jumpForce;
+        if (RB.linearVelocity.y < 0)
+            force -= RB.linearVelocity.y;
+
+        RB.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+        #endregion
+    }
+    #endregion
+
+
+    #region CHECK METHODS
+    public void CheckDirectionToFace(bool isMovingRight)
+    {
+        if (isMovingRight != IsFacingRight)
+            Turn();
+    }
+
+    private bool CanJump()
+    {
+        return LastOnGroundTime > 0 && !IsJumping;
+    }
+
+    private bool CanJumpCut()
+    {
+        return IsJumping && RB.linearVelocity.y > 0;
+    }
+    #endregion
+
+
+    #region EDITOR METHODS
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
+    }
+
+    //Unity Callback, called when the inspector updates
+    private void OnValidate()
+    {
+        //Calculate gravity strength using the formula (gravity = 2 * jumpHeight / timeToJumpApex^2)
+        gravityStrength = -(2 * jumpHeight) / (jumpTimeToApex * jumpTimeToApex);
+
+        //Calculate the rigidbody's gravity scale (ie: gravity strength relative to unity's gravity value, see project settings/Physics2D)
+        gravityScale = gravityStrength / Physics2D.gravity.y;
+
+        //Calculate our run acceleration & deceleration forces using formula: amount = ((1 / Time.fixedDeltaTime) * acceleration) / runMaxSpeed
+        runAccelAmount = (50 * runAcceleration) / runMaxSpeed;
+        runDeccelAmount = (50 * runDecceleration) / runMaxSpeed;
+
+        //Calculate jumpForce using the formula (initialJumpVelocity = gravity * timeToJumpApex)
+        jumpForce = Mathf.Abs(gravityStrength) * jumpTimeToApex;
+
+        #region Variable Ranges
+        runAcceleration = Mathf.Clamp(runAcceleration, 0.01f, runMaxSpeed);
+        runDecceleration = Mathf.Clamp(runDecceleration, 0.01f, runMaxSpeed);
+        #endregion
+    }
+    #endregion
+}
